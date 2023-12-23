9986c4f64ee3f3e8c575126375748de958d753d9
Jonathan Alcantara <jonathan.alcantara@improving.com>
2023-12-22 18:42:44 -0600

    implement accordion component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,51 @@
-const App = () => <h1>Hello React!</h1>
+import { useState } from "react"
+import "./styles.css"
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+]
+
+const App = () => (
+  <div>
+    <Accordion data={faqs} />
+  </div>
+)
+
+const Accordion = ({ data }) => (
+  <div>
+    {data.map((q, index) => (
+      <AccordionItem key={index} num={index} title={q.title} text={q.text} />
+    ))}
+  </div>
+)
+
+const AccordionItem = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleToggle = () => {
+    setIsOpen((isOpen) => !isOpen)
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  )
+}
 
 export default App
